157e8f4cfc4b42cd5aac56c420ffd0640eca0ca8
Adger <112617391+Adgerlee@users.noreply.github.com>
2025-08-28 04:18:06 -0400

    Update tcp-optimize.sh

--- tcp-optimize.sh
+++ tcp-optimize.sh
@@ -99,14 +99,22 @@ fi
 
 # === 🌍 公共 iPerf3 服务器（按区域分组）===
 declare -a SERVERS_LOCAL=(
-    "中国-香港 speedtest.hkg12.hk.leaseweb.net:5203"
     "新加坡    speedtest.singnet.com.sg:5203"
+    "新加坡-新加坡 SGP.proof.ovh.net:5206"
+    "印度-孟买 BOM.proof.ovh.net:5206"
+    "澳洲-悉尼 SYD.proof.ovh.net:5209"
+
 )
 
 declare -a SERVERS_GLOBAL=(
-    "美国-洛杉矶 speedtest.lax12.us.leaseweb.net:5003"
-    "德国-诺德斯特兰 speedtest.wtnet.de:5303"
-    "法国-巴黎 ping.online.net:5203"
+"加拿大-贝尔维尔 BHS.proof.ovh.net:5206"
+"美国-宾州 ERI.proof.ovh.net:5206"
+"美国-俄勒冈 HIL.proof.ovh.net:5206"
+"奥地利-维也纳 vin.proof.ovh.net:5208"
+"法国-鲁贝 RBX.proof.ovh.net:5206"
+"法国-格勒诺布尔 GRA.proof.ovh.net:5206"
+"法国-斯特拉斯堡 SBG.proof.ovh.net:5206"
+"法国-巴黎 ping.online.net:5203"
 )
 
 # 合并用于测速
